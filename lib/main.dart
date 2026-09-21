81ae52fb7f2a9666677b4f2730fb85d1bee497cb
angeljo0801 <angeljo0801@gmail.com>
2026-09-21 18:32:36 -0400

    Nexus: run automatic backup after startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 
 import 'src/app.dart';
+import 'src/core/services/backup_service.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const NexusApp());
+  try {
+    await NexusBackupService.autoBackupIfDue();
+  } catch (_) {}
 }
