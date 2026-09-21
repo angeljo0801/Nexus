8804fb0f7d39928b6a108eea5b007e65623fe225
angeljo0801 <angeljo0801@gmail.com>
2026-09-21 18:34:25 -0400

    Nexus: fix backup analyzer warning

--- lib/src/core/services/backup_service.dart
+++ lib/src/core/services/backup_service.dart
@@ -1,7 +1,5 @@
 import 'dart:convert';
 import 'dart:io';
-import 'dart:typed_data';
-
 import 'package:archive/archive.dart';
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
@@ -154,7 +152,6 @@ class NexusBackupService {
     }
 
     final bytes = ZipEncoder().encode(archive);
-    if (bytes == null) throw Exception('Could not create Nexus backup.');
     return Uint8List.fromList(bytes);
   }
 
